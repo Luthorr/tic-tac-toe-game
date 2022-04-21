7874316dd209445c5f08ed34da80d24b82743d3c
Przemysław Stupak <therixmeisterr@gmail.com>
2022-04-21 15:42:10 +0200

    Initial project, SCSS configuration

---
+++ my-app/src/components/organism/Game/Game.tsx
@@ -0,0 +1,7 @@
+import styles from './Game.module.scss';
+
+const Game = () => {
+  return <div className={styles.container}>Game</div>;
+};
+
+export default Game;
---
+++ my-app/src/views/App.tsx
@@ -0,0 +1,11 @@
+import Game from '../components/organism/Game/Game';
+
+const App = () => {
+  return (
+    <>
+      <Game />
+    </>
+  );
+};
+
+export default App;
